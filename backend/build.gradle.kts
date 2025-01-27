5e9e57f620b09d62b7d15bab6b20e5c3eb61c95d
jihwooon <jihwooon@gmail.com>
2025-01-27 20:44:33 +0900

    Spring Boot 버전 다운그레이드 및 불필요한 리포지토리 제거
    Spring Boot 버전을 3.5.0-M1에서 3.4.2로 다운그레이드하고, 불필요한 리포지토리를 제거했습니다.
    
    주요 변경사항:
    1. Spring Boot 버전을 3.5.0-M1에서 3.4.2로 변경
    2. build.gradle.kts 파일에서 milestone 리포지토리 제거
    3. settings.gradle.kts 파일에서 pluginManagement 섹션 제거

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
 	java
-	id("org.springframework.boot") version "3.5.0-M1"
+	id("org.springframework.boot") version "3.4.2"
 	id("io.spring.dependency-management") version "1.1.7"
 }
 
@@ -21,7 +21,6 @@ configurations {
 
 repositories {
 	mavenCentral()
-	maven { url = uri("https://repo.spring.io/milestone") }
 }
 
 dependencies {
