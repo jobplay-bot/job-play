4c3055d18d5e878838813c6c114edddf00c364ff
jihwooon <jihwooon@gmail.com>
2025-01-27 20:26:53 +0900

    SpringBoot backend 환경 구성으로 생성하라
    주요 변경사항:
    1. SpringBoot 프로젝트 생성하였습니다.

---
+++ backend/build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.5.0-M1"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.jobplay"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(23)
+	}
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+	maven { url = uri("https://repo.spring.io/milestone") }
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	compileOnly("org.projectlombok:lombok")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	runtimeOnly("com.mysql:mysql-connector-j")
+	annotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
