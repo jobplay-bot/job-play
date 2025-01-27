e1430b2f6630770213db5a9ef38be1ad7f80ea71
jihwooon <jihwooon@gmail.com>
2025-01-27 20:25:12 +0900

    nestjs 환경 설정 폴더를 삭제하라
    주요변경사항:
    1. Backend 환경설정 변경으로 Spring Boot로 변경하였다.

--- backend/scripts/run_app.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-. "$(dirname "$0")"/common.cfg
-
-docker_compose --profile apps down
-docker_compose --profile apps up -d --build
--- backend/src/apps/applications/study/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './study.module'
-export * from './study.entity'
-export * from './study.service'
-export * from './study.controller'
