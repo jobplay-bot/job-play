c4205e5788a5079d3ebbd55f96ce6aed56d342b7
wsdsdssa <23dkssud@naver.com>
2025-04-13 19:41:03 +0900

    mockup data 적용

--- front/app/job-celebration-from.tsx
+++ front/app/job-celebration-from.tsx
@@ -1,17 +1,23 @@
-import JobDetailForm from "@/app/job-detail-form";
+"use client";
+
+import JobCard from "@/app/JobCard";
 import { getJobDetail } from "./api/job-detail.api";
+import { useRouter } from "next/navigation";
 
 export default function JobCelebrationForm() {
+  const router = useRouter();
   const jobs = getJobDetail();
+  
+  const handleApplyClick = (id: number) => {
+    router.push(`/careers/${id}`); // "/careers" 경로로 이동
+  };
 
   return (
     <div className="min-h-screen">
       <div className="mx-auto max-w-md space-y-8">
-        {
-          jobs.map((job, index) => (
-            <JobDetailForm key={index}/>
-          ))
-        }
+        {jobs.map((job) => (
+        <JobCard key={job.id} job={job} onApplyClick={handleApplyClick} />
+        ))}
       </div>
     </div>
   );
