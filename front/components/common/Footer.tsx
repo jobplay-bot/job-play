0307dd37d424b339763001bd4a2e9d105d84f306
wsdsdssa <23dkssud@naver.com>
2025-04-13 17:50:30 +0900

    commit test

--- front/components/common/Footer.tsx
+++ front/components/common/Footer.tsx
@@ -6,7 +6,7 @@ export const Footer = () => {
   return (
     <footer className="pt-8 text-center text-sm text-gray-500">
       © JOBPLAY
-      <div className="mt-2">서비스 이용약관 | 개인정보 처리방침</div>
+      <div className="mt-2">서비스 이용약관 | 개인정보 처리방침 </div>
     </footer>
   );
 };
